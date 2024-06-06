78f015812bd8516f2528dd2dfeaf84afa78108ac
sanjaykumaryada <2408sanjaykumar@gmail.com>
2024-06-06 13:46:09 +0530

    add:button and input

--- packages/examples/src/App.tsx
+++ packages/examples/src/App.tsx
@@ -1,11 +1,18 @@
-import { Button } from "master-components";
+import Input from "master-components/src/lib/core-components/Input";
+import Button from "../../master-components/src/lib/core-components/Button"
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-
+  const [val, setValue] = useState("");
   return (
     <>
-      <Button />
+      {val}
+      <Button label="Button" toolTipProps={{ content: "this is button tooltip", placement: "right" }} />
+      <div className="root">
+        <Button label="button with tooltip" toolTipProps={{ content: "this is button tooltip", placement: "top" }} />
+      </div>
+      <Input value={val} onChange={(e) => setValue(e.target.value)} label="input" startIcon={<>Hii</>} endIcon={<>Hello</>} helperText="helper text" />
     </>
   );
 }
