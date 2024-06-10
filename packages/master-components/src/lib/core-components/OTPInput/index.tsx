ceec774d8224d46037eaa1fb42a6e71b08527b52
sanjaykumaryada <2408sanjaykumar@gmail.com>
2024-06-10 12:57:37 +0530

    types issue in otpinput

--- packages/master-components/src/lib/core-components/OTPInput/index.tsx
+++ packages/master-components/src/lib/core-components/OTPInput/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { type ReactNode, useEffect, useState } from 'react';
 import "./index.css";
 
 type AllowedInputTypes = 'password' | 'text' | 'number';
