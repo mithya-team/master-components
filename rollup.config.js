de4038165e1e6be0408d0fe896c5bce078806fea
sanjaykumaryada <2408sanjaykumar@gmail.com>
2024-06-25 13:02:25 +0530

    fix build issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,33 @@
+// rollup.config.js
+import terser from "@rollup/plugin-terser";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
-import external from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
-import packageJson from "./package.json" assert { type: "json" };
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import css from "rollup-plugin-import-css";
+
+import pkg from "./package.json" assert { type: "json" };
 
 export default [
   {
     input: "src/index.ts",
     output: [
       {
-        file: packageJson.main,
+        file: pkg.main,
         format: "cjs",
         sourcemap: true,
-        name: "react-ts-lib",
       },
       {
-        file: packageJson.module,
+        file: pkg.module,
         format: "esm",
         sourcemap: true,
       },
     ],
     plugins: [
-      postcss(),
-      external(),
-      resolve({
-        browser: true,
-      }),
+      css(),
+      peerDepsExternal(),
+      resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
@@ -37,7 +36,7 @@ export default [
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    external: [/\.css$/],
     plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
   },
 ];
